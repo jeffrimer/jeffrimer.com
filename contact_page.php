f2a75c29fad1cc87ff85c824cd056c8f0aec866f
Jeff Rimer <jmrimer@gmail.com>
2014-01-30 22:42:57 -0500

    Adding all files

---
+++ contact_page.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title></title>
+  <meta name="description" content="">
+
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="includes/js/libs/modernizr-2.5.3.min.js"></script>
+</head>
+<body>
+  <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+  <?php include('includes/php/header.php'); ?>
+  <div id="main">
+   <div id="content">
+      <p class="top">If interested in contacting me, please fill out the small form below. I will return your message as soon as time permits. Thank you!</p>
+      <form action="contact.php" method="post" id="contact">
+        <div class="hideOnSubmit">
+          <label for="cf_name" class="contactLabel">Your name</label>
+          <input type="text" class="contactInput" name="cf_name" />
+        </div>
+        <div class="hideOnSubmit">
+          <label for="cf_email" class="contactLabel">Your e-mail</label>
+          <input type="text" class="contactInput" name="cf_email" />
+        </div>
+        <div class="hideOnSubmit">
+          <label for="cf_message" class="contactLabel">Message</label>
+          <textarea name="cf_message"></textarea>
+        </div>
+        <div class="hideOnSubmit">
+          <input type="submit" id="contactSubmit" class="submit" value="Send">
+          <input type="reset" value="Clear" class="submit">
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <?php include('includes/php/footer.php'); ?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title></title>
+  <meta name="description" content="">
+
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="includes/js/libs/modernizr-2.5.3.min.js"></script>
+</head>
+<body>
+  <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+  <?php include('includes/php/header.php'); ?>
+  <div id="main">
+    <div id="content">
+      <p class="top">Hi! I'm Jeff Rimer and this is my little home on the Internet. I'm a front end developer but love to delve into the code behind a site (PHP) and some other Object Oriented languages (C++, Java). I'm working on my first Android app, buffing out this website little by little, and constantly studying to get better at my job.</p>
+      <p>The purpose of this site is to provide a housing for my pet projects and fun excursions. Since this is a semi-portfolio site, I'll have a copy of my <a href="/resume.php" title="My Current Resume">current resume</a> here as well.</p>
+      <p>As a quick introduction, I have worked for a year and a half at <a href="http://www.tmprod.com" title="Trademark Productions" target="_blank" rel="external">Trademark Productions</a> as a front end developer. I'm currently developing my first Android app and coming up with ideas for fun projects to do here.</p>
+    </div>
+  </div>
+
+  <?php include('includes/php/footer.php'); ?>
+
+</body>
+</html>
