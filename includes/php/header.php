694ff399207e97d0137847c99f9b9022060d3a3c
Jeff Rimer <jmrimer@gmail.com>
2014-02-01 20:02:40 -0500

    Making root directory work, changing URL of contact page

--- includes/php/header.php
+++ includes/php/header.php
@@ -4,8 +4,8 @@
         <ul id="nav">
             <li><a href="/index.php" title="Back to Homepage">Home</a></li>
             <li><a href="/resume.php" title="My Resume">Resume</a></li>
-            <li><a href="/portfolio.php" title="My Portfolio">Portfolio</a></li>
-            <li><a href="/contact_page.php" title="Contact Me">Contact Me</a></li>
+            <!--<li><a href="/portfolio.php" title="My Portfolio">Portfolio</a></li>-->
+            <li><a href="/contact-me.php" title="Contact Me">Contact Me</a></li>
         </ul>
     </div>
 </header>
