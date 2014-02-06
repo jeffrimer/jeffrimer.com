77de3a4338f8e56a0b4b630965b0ab70c3ceec05
Jeff Rimer <jmrimer@gmail.com>
2014-02-05 22:06:46 -0500

    Contact form working

--- contact.php
+++ contact.php
@@ -1,38 +1,37 @@
 <?php
 
-$field_name = $_POST['cf_name'];
-$field_email = $_POST['cf_email'];
-$field_message = $_POST['cf_message'];
+ 	require_once 'google/appengine/api/mail/Message.php';
+    use google\appengine\api\mail\Message;
 
+	if( $_POST['cf_name'] == '' || $_POST['cf_email'] == '' || $_POST['cf_message'] == '' ) {
 
-$mail_to = 'jmrimer@gmail.com';
-$subject = 'Message from a site visitor '.$field_name;
+	echo '<script language="javascript" type="text/javascript">
+        alert(\'Please fill all fields.\');
+        window.location = \'contact-me.php\';
+		</script>';
+		exit(0);
+	}
 
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Message: '.$field_message;
+    $emailBody = $_POST['cf_message'] . ' -- ' . $_POST['cf_name'] . ' - ' . $_POST['cf_email'];
 
-$headers = 'From: '.$cf_email."\r\n";
-$headers .= 'Reply-To: '.$cf_email."\r\n";
+    $mail_options = [
+       "sender" => 'jmrimer@gmail.com',
+       "to" => 'jmrimer@gmail.com',
+       "subject" => "Email Received from JeffRimer.com",
+       "textBody" => $emailBody
+    ];
 
-if ( $field_name != '' && $field_email != '' && $field_message != '' ) {
-    $mail_status = mail($mail_to, $subject, $body_message, $headers);
-} else { ?>
-    <script language="javascript" type="text/javascript">
-        alert('Please fill all fields.');
-        window.location = 'contact_page.php';
-    </script>
-<?php }
-
-if ($mail_status) { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Thank you for the message. I will get back with you as soon as I can.');
-		window.location = 'contact_page.php';
-	</script>
-<?php
-} else { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Message failed. Please send an email directly to me at jmrimer@gmail.com.');
-		window.location = 'contact_page.php';
-	</script>
-<?php } ?>
+    try {
+       $message = new Message($mail_options);
+       $message->send();
+       echo '<script language="javascript" type="text/javascript">
+		  alert(\'Thank you for the message. I will get back with you as soon as I can.\');
+		  window.location = \'contact-me.php\';
+	   </script>';
+    } catch (InvalidArgumentException $e) {
+    	echo '<script language="javascript" type="text/javascript">
+		   alert(\'Message failed. Please send an email directly to me at jmrimer@gmail.com.\');
+		   window.location = \'contact-me.php\';
+	    </script>';
+       echo $e;
+    }
